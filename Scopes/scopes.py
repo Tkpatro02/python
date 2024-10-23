76bb9cc6f6b8ce2bb6361667e0b0faa4f394d8a7
triyank.patro <triyank.patro@gmail.com>
2024-10-24 00:23:57 +0530

    second

---
+++ Scopes/scopes.py
@@ -0,0 +1,17 @@
+username="Tamdi Tamdi"
+
+# def func():
+#     username="chai"
+#     print(username)
+
+# func()
+# print(username)
+
+x=99
+
+def fun(y):
+    x=44
+    z=x+y
+    return z
+result=fun(1)
+print(result)
---
+++ oops/ques1.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self,brand,model):
+        self.brand=brand
+        self.model=model
+
+my_car=Car("Hyundai","Creata")
+print(my_car.brand)
+print(my_car.model)
+
+my_car_new=Car("Mercedes","Lx44W")
+print(my_car_new.brand)
+print(my_car_new.model)
